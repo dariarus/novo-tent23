e6e98df67ca9a43cfd678705eee67c13edfea944
Daria Rusanova <rusanova_dv@mail.ru>
2025-03-17 13:13:59 +0300

    feature: настроить плавны скролл страницы при клике на ссылки навигации

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Переход на соц.сети при клике на кнопки в блоке "Contacts"
 function openInstagram() {
   window.open("https://www.instagram.com/novo_tent/?igsh=MTNobHdxYWs1eW9kYw%3D%3D&utm_source=qr#");
 }
@@ -13,3 +14,25 @@ function openMail() {
 document.getElementById("instagramButton").addEventListener("click", openInstagram);
 document.getElementById("whatsAppButton").addEventListener("click", openWhatsApp);
 document.getElementById("emailButton").addEventListener("click", openMail);
+
+// Плавный скролл страницы
+document.querySelectorAll('.navigation__link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1); // Убираем #
+    const targetElement = document.getElementById(targetId);
+
+
+    const headerHeight = document.querySelector('header').offsetHeight;
+
+    if (targetElement) {
+      const offsetTop = targetElement.getBoundingClientRect().top + window.scrollY - headerHeight; // Учитываем отступ для фиксированного меню
+
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
