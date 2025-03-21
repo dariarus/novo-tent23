498bb3a6b215ef13c9f1b7374f79ce7be4fc0f0d
Daria Rusanova <rusanova_dv@mail.ru>
2025-03-21 18:48:25 +0300

    feature: настроить эффект hover для иконок в хедере и внутри кнопок контактов с помощью скрипта

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,26 @@
+// Эффект hover у иконок в блоках "Header" и "Contacts"
+const headerIconPlace = document.querySelector('.header__contacts-icons-wrapper')
+headerIconPlace.querySelectorAll('.contacts-icon').forEach((headerIcon) => {
+  headerIcon.addEventListener('mouseenter', () => {
+    headerIcon.classList.add('contacts-icon_place_header');
+  });
+
+  headerIcon.addEventListener('mouseleave', () => {
+    headerIcon.classList.remove('contacts-icon_place_header');
+  });
+});
+
+document.querySelectorAll('.button').forEach(button => {
+  const buttonIcon = button.querySelector('.contacts-icon');
+  button.addEventListener('mouseenter', () => {
+    buttonIcon.classList.add('contacts-icon_place_button');
+  });
+
+  button.addEventListener('mouseleave', () => {
+    buttonIcon.classList.remove('contacts-icon_place_button');
+  });
+});
+
 // Переход на соц.сети при клике на кнопки в блоке "Contacts"
 function openInstagram() {
   window.open("https://www.instagram.com/novo_tent/?igsh=MTNobHdxYWs1eW9kYw%3D%3D&utm_source=qr#");
@@ -52,7 +75,7 @@ document.querySelectorAll('.product-card').forEach((productCard) => {
 
 // Переход к WhatsApp при клике на "Заказать" в блоке "Products"
 document.querySelectorAll(".button_colored").forEach(button => {
-  button.addEventListener("click", function() {
+  button.addEventListener("click", function () {
     openWhatsApp();
   });
 });
